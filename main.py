965dd4b7fd116bfb2d9aeb7661db69d59dcff3d0
JRudeeee <jarred.rudolph@gmail.com>
2025-11-10 14:41:15 +0800

    CH2 L6

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_character_count
 
 def get_book_text(filepath):
     file_output = None
@@ -9,7 +10,9 @@ def get_book_text(filepath):
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_contents)
+    character_count = get_character_count(book_contents)
     print(f"Found {word_count} total words")
+    print(character_count)
     
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -7,3 +7,14 @@ def get_num_words(book_contents):
         word_count += 1
     
     return word_count
+
+def get_character_count(book_contents):
+    book_contents = book_contents.lower()
+    characters = {}
+    for char in book_contents:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    
+    return characters
