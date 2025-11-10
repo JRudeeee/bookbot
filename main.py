e0ca4bd72911729ee37ba19c7493a50fec5ffbce
JRudeeee <jarred.rudolph@gmail.com>
2025-11-10 15:13:08 +0800

    Completed bookbot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 from stats import get_num_words
 from stats import get_character_count
+from stats import sort_char_dict
 
 def get_book_text(filepath):
     file_output = None
@@ -8,11 +10,25 @@ def get_book_text(filepath):
     return file_output
     
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_contents = get_book_text(sys.argv[1])
     word_count = get_num_words(book_contents)
     character_count = get_character_count(book_contents)
-    print(f"Found {word_count} total words")
-    print(character_count)
+    sorted_characters = sort_char_dict(character_count)
+    
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {sys.argv[1]}...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------""")
+    for char in sorted_characters:
+        if char["char"].isalpha():
+            temp_char = char["char"]
+            temp_num = char["num"]
+            print(f"{temp_char}: {temp_num}")
     
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -18,3 +18,16 @@ def get_character_count(book_contents):
             characters[char] = 1
     
     return characters
+def sort_on(chars):
+    return chars["num"]
+
+def sort_char_dict(character_dict):
+    sorted_list = []
+
+    for char in character_dict:
+        temp_dict = {"char": char, "num": character_dict[char]}
+        sorted_list.append(temp_dict)
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+        
+    return sorted_list
