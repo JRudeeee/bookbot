b8d825dae73dfd54436d80c59579266b41588404
JRudeeee <jarred.rudolph@gmail.com>
2025-11-10 14:28:05 +0800

    CH2 L4

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_output = f.read()
     return file_output
+
+def get_num_words(book_contents):
+    words = []
+    words = book_contents.split()
+    word_count = 0
+    
+    for word in words:
+        word_count += 1
+    
+    return word_count
     
 def main():
-    output = get_book_text("books/frankenstein.txt")
-    print(output)
+    book_contents = get_book_text("books/frankenstein.txt")
+    word_count = get_num_words(book_contents)
+    print(f"Found {word_count} total words")
     
 if __name__ == "__main__":
     main()
